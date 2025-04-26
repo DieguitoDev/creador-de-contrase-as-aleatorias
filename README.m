9c3897d1805153e9f06670963ef4c437c0aeef18
DieguitoDev <diego.jaimes@sanpedro.edu.co>
2025-04-26 12:25:08 -0500

    Update README.m

--- README.m
+++ README.m
@@ -1,1 +1,3 @@
-
+este proyecto trata sobre un creador de contraseñas creadas de forma aleatoria,
+ dependiendo de la magnitud o la cantidad de caracteres que desee el 
+ usuario. Ademas, incluye letras mayusculas, minusculas y simbolos que hacen que sea la contraseña segura.
